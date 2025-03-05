67e202e377ba2882c57a0bca40226bdce21654c5
Ivan <xsardesxsardes@gmail.com>
2025-03-05 17:45:28 +0800

    7 commit

--- src/test/java/com/lopinivan/walletservice/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.lopinivan.walletservice;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
